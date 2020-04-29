ebc02b377b1289d6521c7c41e14951e9d08f8361
nikkyxiong <nikkyxiong97@gmail.com>
2020-04-29 18:56:44 -0400

    dashboard2.0

--- Dashboard/dashboard.R
+++ Dashboard/dashboard.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(shinyWidgets)
+library(shinydashboard)
 library(readr)
 library(dplyr)
 library(tidyr)
@@ -12,68 +14,25 @@ library(ggthemes)
 library(scales)
 library(plotly)
 library(stringr)
+library(TTR)
+library(tm)
+library(SnowballC)
+library(RColorBrewer)
+library(tidytext)
+library(stringr)
+library(tidyr)
+library(geojsonio)
+library(usmap)
+library(tidycensus)
+library(gapminder)
 
 ########################
 # Read in Cleaned Data #
 ########################
 
 df_raw <- read_csv('complaints.csv') 
-
-##############
-#     UI     #
-##############
-
-ui = fluidPage(
-    theme = "style.css",
-    div(style = "padding: 1px 0px; width: '100%'",
-        titlePanel(
-            title = "",
-            windowTitle = "Dashboard for Comsumer Complaints"
-        )
-    ),
-    navbarPage(
-        # Application title.
-        title = div(span(
-            "Dashboard for Comsumer Complaints",
-            style = "position: relative; top: 50%; transform: translateY(-50%);")),
-        
-        tabPanel(
-            "EDA Overview",
-            tags$h1("Overview of Project for Consumer Complaint"),
-            tags$p(HTML('&nbsp;')),
-            tags$h2("Intro"),
-            tags$p(HTML('&nbsp;')),
-            tags$p(tags$a(href="https://www.consumerfinance.gov/data-research/consumer-complaints/", "Click here check out our data")),
-            tags$p(tags$a(href="https://github.com/QMSS-Spring-2020", "Click here to see the code")),
-            tags$p(HTML('&nbsp;')),
-            tags$p("by Jiaqing Ge, Nikky Xiong, Richa Gupta"),
-            tags$p(HTML('&nbsp;')),
-            tags$h2("Overall Number of Complaints throughout a Year"),
-            plotlyOutput("linechart", width = 800, height = 500),
-            tags$p(HTML('&nbsp;')),
-            dataTableOutput('ex1'),
-            
-            sidebarLayout(
-                sidebarPanel(
-                    conditionalPanel(
-                        'input.dataset === "df_new"',
-                        checkboxGroupInput("show_vars", "Columns to show:",
-                                           names(df_new), selected = names(df_new))
-                    )
-                )
-            ),
-        
-        tabPanel(
-            "Text Mining and Visualization for Narrative",
-            tabPanel("", includeHTML("model2017_2.html"))
-        ),
-        
-        tabPanel(
-            "Unsupervised Learning - LDA",
-            tabPanel("", includeHTML("LDA.html"))
-        )
-    )
-)))
+complaints_test <- readRDS('complaints_sub.rds')
+complaints_narrative_corp <- readRDS('complaints_narrative_corp.rds')
 
 ##########################
 #   Data Preprocessing   #
@@ -104,35 +63,247 @@ pretty_headers <-
     gsub("[.]", " ", colnames(df_new)) %>%
     str_to_title()
 
+# More
+complaints_test <- complaints_test %>%
+    mutate(year = as.integer(substr(Date.received, start = 1, stop = 4))) %>%
+    mutate(month = as.integer(substr(Date.received, start = 6, stop = 7))) %>%
+    mutate(day = as.integer(substr(Date.received, start =9 , stop = 10)))
+complaints_test$Date.received <- ymd(complaints_test$Date.received)
+# create overall complaint level graphs
+complaints_pattern <- function(x){
+    if (x=="All"){
+        p_ <- complaints_test %>% 
+            mutate(date = as.POSIXct(paste(month , day , sep = "." )  , format = "%m.%d" )) %>%
+            group_by(date) %>%
+            summarise(number_of_complaints = n()) %>%
+            ggplot(aes(x = date, y = number_of_complaints))+ geom_point()+ylab("Number of complaints")+
+            theme_economist()+
+            scale_x_datetime(labels=  date_format("%b-%d"),date_breaks = '1 month')+
+            geom_smooth(lwd=1, se=FALSE,color = 'red')+
+            geom_line(aes(x=date, y=SMA(number_of_complaints,10), color = 'red'))+ 
+            theme(legend.position="none")+
+            ggtitle(paste("Number of Complaints throughout a Year (Overall level):",simpleCap(tolower(x)), sep = ' '))
+    } else{
+        p_ <-  complaints_test %>% 
+            filter(Company %in% x)  %>%
+            mutate(date = as.POSIXct(paste(month , day , sep = "." )  , format = "%m.%d" )) %>%
+            group_by(date) %>%
+            summarise(number_of_complaints = n()) %>%
+            ggplot(aes(x = date, y = number_of_complaints))+ geom_point()+ylab("Number of complaints")+
+            theme_economist()+
+            scale_x_datetime(labels=  date_format("%b-%d"),date_breaks = '1 month')+
+            geom_smooth(lwd=1, se=FALSE,color = 'red')+
+            theme(legend.position="none")+
+            ggtitle(paste("Number of Complaints throughout a Year of selected companies"))+theme(plot.title = element_text(size=10))}
+    ggplotly(p_)}
+company_names <- complaints_test %>%
+    select(Company)%>%
+    distinct()
+Top_10_comanies <- complaints_test %>%
+    group_by(Company) %>%
+    summarise(number_of_complaints = n())%>%
+    arrange(desc(number_of_complaints))%>%
+    head(10)%>%
+    select(Company) # This line select the companies with most complaints
+Top_10_comanies_plot <- complaints_test %>%
+    filter(Company %in% Top_10_comanies$Company) %>%
+    group_by(Company,Product)%>%
+    summarise(number_of_complaints = n())%>%
+    ungroup()%>%
+    mutate(Company = factor(Company, levels=Top_10_comanies$Company))%>%
+    ggplot(aes(fill=Product, y=number_of_complaints, x=Company)) + 
+    geom_bar(position="stack", stat="identity")+ylab('Nunber of Complaints by Company')+coord_flip()+theme(legend.position="right")+guides(fill=guide_legend(nrow=15))
+Top_10_comanies_plot <- ggplotly(Top_10_comanies_plot,width = 1500, height = 500, type = 'bar')
+Top_10_product <- complaints_test %>%
+    group_by(Product) %>%
+    summarise(number_of_complaints = n())%>%
+    arrange(desc(number_of_complaints))%>%
+    head(10)%>%
+    select(Product) # This line select the companies with most complaints
+Top_10_product_plot <- complaints_test %>%
+    filter(Product %in% Top_10_product$Product) %>%
+    group_by(Product,Submitted.via)%>%
+    summarise(number_of_complaints = n())%>%
+    ungroup()%>%
+    mutate(Product = factor(Product, levels=Top_10_product$Product))%>%
+    ggplot(aes(fill=Submitted.via, y=number_of_complaints, x=Product)) + 
+    geom_bar(position="stack", stat="identity")+ylab('Nunber of Complaints by Product')+coord_flip()+theme(legend.position="bottom")
+Top_10_product_plot <- ggplotly(Top_10_product_plot,width = 1200, height = 500, type = 'bar')
+share_of_products_ <- complaints_test %>%
+    group_by(Product) %>%
+    summarize(count = n()) %>%
+    plot_ly(labels = ~Product, values = ~count,width = 1200, height = 500) %>%
+    add_pie(hole = 0.6) %>%
+    layout(title = "Share of Products",  showlegend = T,
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),legend = list(x = 100, y = 0.5))
+
+top_word_response <- complaints_narrative_corp %>%
+    arrange(desc(tf_idf)) %>%
+    mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+    group_by(Company.response.to.consumer) %>% 
+    top_n(10) %>% 
+    ungroup() %>%
+    ggplot(aes(word, tf_idf, fill = Company.response.to.consumer)) +
+    geom_col(show.legend = FALSE) +
+    labs(x = NULL, y = "tf-idf") +
+    facet_wrap(~Company.response.to.consumer, ncol = 2, scales = "free") +
+    coord_flip()
+top_word_response <- ggplotly(top_word_response,width = 1000, height = 1000, type = 'bar')
+company_response <- complaints_test %>%
+    select(Company.response.to.consumer)%>%
+    distinct()
+wordcloud_rep <- repeatable(wordcloud)
+word_cloud <- function(max_word,freq,company_res){
+    sub_set <- complaints_narrative_corp %>%
+        filter(Company.response.to.consumer == company_res)
+    print(wordcloud_rep(words = sub_set$word, freq = sub_set$n,
+                        min.freq = freq,max.words=max_word, random.order=FALSE, rot.per=0.35, 
+                        colors=brewer.pal(8, "Dark2")))                  
+}
+states <- 
+    geojson_read( 
+        x = "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json"
+        , what = "sp"
+    )
+bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
+pal <- colorBin("YlOrRd", domain = states$density, bins = bins)
+
+labels <- sprintf(
+    "<strong>%s</strong><br/>%g people / mi<sup>2</sup>",
+    states$name, states$density
+) %>% lapply(htmltools::HTML)
+
+leaflet(states) %>%
+    setView(-96, 37.8, 4) %>%
+    addProviderTiles("MapBox", options = providerTileOptions(
+        id = "mapbox.light",
+        accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+    addPolygons(
+        fillColor = ~pal(density),
+        weight = 2,
+        opacity = 1,
+        color = "white",
+        dashArray = "3",
+        fillOpacity = 0.7,
+        highlight = highlightOptions(
+            weight = 5,
+            color = "#666",
+            dashArray = "",
+            fillOpacity = 0.7,
+            bringToFront = TRUE),
+        label = labels,
+        labelOptions = labelOptions(
+            style = list("font-weight" = "normal", padding = "3px 8px"),
+            textsize = "15px",
+            direction = "auto")) %>%
+    addLegend(pal = pal, values = ~density, opacity = 0.7, title = NULL,
+              position = "bottomright")
+
+##############
+#     UI     #
+##############
+
+ui = fluidPage(
+    theme = "style.css",
+    div(style = "padding: 1px 0px; width: '100%'",
+        titlePanel(
+            title = "",
+            windowTitle = "Dashboard for Comsumer Complaints"
+        )
+    ),
+    navbarPage(
+        # Application title.
+        title = div(span(
+            "Dashboard for Comsumer Complaints",
+            style = "position: relative; top: 50%; transform: translateY(-50%);")),
+        
+        tabPanel(
+            "EDA Overview",
+            tags$h1("Overview of Project for Consumer Complaints"),
+            tags$h4("by Jiaqing Ge, Nikky Xiong, Richa Gupta"),
+            tags$p(HTML('&nbsp;')),
+            tags$p(tags$a(href="https://www.consumerfinance.gov/data-research/consumer-complaints/", "Click here check out our data")),
+            tags$p(tags$a(href="https://github.com/QMSS-Spring-2020", "Click here to see the code")),
+            tags$p(HTML('&nbsp;')),
+            dataTableOutput('ex1', width = 900, height = 400),
+        ),
+        
+        tabPanel("Overall Complaint level through out a year",
+                 
+                 sidebarLayout(
+                     sidebarPanel(
+                         
+                         pickerInput("level_select", "Level:",   
+                                     choices = c("Overall","Company"), 
+                                     selected = c("Company"),
+                                     multiple = FALSE),
+                         
+                         pickerInput("company_select", "Company",   
+                                     choices = as.character(company_names$Company), 
+                                     options = list(`actions-box` = TRUE, `none-selected-text` = "Please make a selection!"),
+                                     selected = c('BANK OF AMERICA, NATIONAL ASSOCIATION'),multiple = TRUE)),
+                     
+                     mainPanel(
+                         tabPanel('Complaint Throughout a Year',plotlyOutput("complaints_pattern_plot")))
+                 )
+        ),
+        
+        tabPanel("Shares of Complaints",
+                 mainPanel(
+                     tabsetPanel(
+                         tabPanel('Top 10 Companies of number of complaints',plotlyOutput("Top_10_comanies_plot")),
+                         tabPanel('Top 10 Products of highest number of complaints',plotlyOutput("Top_10_product_plot")),
+                         tabPanel('Share of Complaints by Product',plotlyOutput("share_of_products_")))
+                 )
+        ),
+        
+        tabPanel("Company response text mining",
+                 
+                 sidebarLayout(
+                     sidebarPanel(
+                         
+                         
+                         sliderInput("max_word", step = 1,
+                                     "Max number of words to show",
+                                     min = 1,
+                                     max = 500,
+                                     value = 100),
+                         pickerInput("response", "Company response to consumer",   
+                                     choices = as.character(company_response$Company.response.to.consumer), 
+                                     selected = c('Closed with monetary relief'),
+                                     multiple = FALSE),
+                         sliderInput("freq",
+                                     "Minimum Frequency:",
+                                     min = 1,  max = 1000, value = 100),
+                     ),
+                     
+                     mainPanel(
+                         tabsetPanel(
+                             tabPanel("Frequentest words by Company response", plotlyOutput("top_word_response")),
+                             tabPanel("Word cloud", plotOutput("word_cloud_plot"))
+                         )
+                     )
+                 )
+        ),
+        
+        tabPanel(
+            "Text Mining and Visualization for Narrative",
+            tabPanel("", includeHTML("model2017_2.html"))
+        ),
+        
+        tabPanel(
+            "Unsupervised Learning - LDA",
+            tabPanel("", includeHTML("LDA.html"))
+        )
+    )
+)
+
 ##################
 #     Server     #
 ##################
 
-server = function(input, output, session) {
-    
-    # EDA Overview #
-    
-    p <- df %>% 
-        mutate(date = as.POSIXct(paste(month , day , sep = "." ), 
-                                 format = "%m.%d" )) %>%
-        group_by(date, `Company response to consumer`) %>%
-        summarise(number_of_complaints = n())
-    
-    pp <- ggplot(p, aes(x = date, y = number_of_complaints,
-                        color = `Company response to consumer`)) +
-        geom_line()+
-        ylab("Number of complaints") +
-        theme_tufte() +
-        scale_x_datetime(labels= date_format("%b"), 
-                         date_breaks = '1 month') + 
-        theme(plot.title = element_text(size=14, face="bold", hjust = 0.5),
-              legend.text = element_text(size=8),
-              legend.title = element_text(size=8),
-              axis.text.x = element_text(angle = 45)) +
-        ggtitle("Overall Number of Complaints throughout a Year")
-    
-    output$linechart <- renderPlotly(pp)
-    
+server = function(input, output, session){
     output$ex1 <- DT::renderDataTable(
         DT::datatable(df_new, 
                       caption = 'Table 1: This is a simple data table for the complaints.',
@@ -141,12 +312,46 @@ server = function(input, output, session) {
                       colnames = pretty_headers,
                       filter = list(position = "top"),
                       options = list(
+                          pageLength = 5,
                           dom = "Bfrtip",
                           buttons = I("colvis"),
                           language = list(sSearch = "Filter:")
                       ),
                       extensions = c("Buttons", "Responsive"))
     )
+    
+    # update company selections
+    observeEvent(input$level_select, {
+        if (input$level_select=="Overall") {
+            updatePickerInput(session = session, inputId = "company_select", 
+                              choices = "All", selected = "All")
+        }
+        
+        if (input$level_select=="Company") {
+            updatePickerInput(session = session, inputId = "company_select", 
+                              choices = as.character(company_names$Company), 
+                              selected = c('BANK OF AMERICA, NATIONAL ASSOCIATION'))
+        }
+    }, ignoreInit = TRUE)
+    
+    output$complaints_pattern_plot <- renderPlotly({
+        complaints_pattern(input$company_select)
+    })
+    output$Top_10_comanies_plot <- renderPlotly({
+        Top_10_comanies_plot
+    })
+    output$Top_10_product_plot <- renderPlotly({
+        Top_10_product_plot
+    })
+    output$share_of_products_ <- renderPlotly({
+        share_of_products_
+    })
+    output$top_word_response <- renderPlotly({
+        top_word_response
+    })
+    output$word_cloud_plot <- renderPlot({
+        word_cloud(input$max_word,input$freq ,input$response)
+    })
 }
 
 #######################
